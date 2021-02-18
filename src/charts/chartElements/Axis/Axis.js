9e82f95f3d941feee80a75c70390f032d577170b
Sam Baguley <hello@sambaguley.com>
2021-02-18 09:47:00 +0000

    Add line ticks

--- src/charts/chartElements/Axis/Axis.js
+++ src/charts/chartElements/Axis/Axis.js
@@ -1,6 +1,6 @@
-import React from "react"
-import PropTypes from "prop-types"
-import * as d3 from 'd3'
+import React from "react";
+import PropTypes from "prop-types";
+import * as d3 from "d3";
 import { dimensionsPropsType } from "../../utils/utils";
 // import { useChartDimensions } from "../../utils/useChartDimensions";
 import "./axis.css";
@@ -8,18 +8,13 @@ import "./axis.css";
 const axisComponentsByDimension = {
   x: AxisHorizontal,
   y: AxisVertical,
-}
+};
 const Axis = ({ dimensions, dimension, ...props }) => {
-  const Component = axisComponentsByDimension[dimension]
-  if (!Component) return null
+  const Component = axisComponentsByDimension[dimension];
+  if (!Component) return null;
 
-  return (
-    <Component
-      dimensions={dimensions}
-      {...props}
-    />
-  )
-}
+  return <Component dimensions={dimensions} {...props} />;
+};
 
 Axis.propTypes = {
   dimension: PropTypes.oneOf(["x", "y"]),
@@ -27,39 +22,49 @@ Axis.propTypes = {
   scale: PropTypes.func,
   label: PropTypes.string,
   formatTick: PropTypes.func,
-}
+};
 
 Axis.defaultProps = {
   dimension: "x",
   scale: null,
   formatTick: d3.format(","),
-}
+};
 
-export default Axis
+export default Axis;
 
-
-function AxisHorizontal ({ dimensions, label, formatTick, scale, ...props }) {
-  const numberOfTicks = dimensions.boundedWidth < 600
-        ? dimensions.boundedWidth / 50
-        : dimensions.boundedWidth / 100
+function AxisHorizontal({ dimensions, label, formatTick, scale, ...props }) {
+  const numberOfTicks =
+    dimensions.boundedWidth < 600
+      ? dimensions.boundedWidth / 50
+      : dimensions.boundedWidth / 100;
 
   const ticks = scale.ticks(numberOfTicks);
 
   return (
-    <g className="Axis AxisHorizontal" transform={`translate(0, ${dimensions.boundedHeight})`} {...props}>
-      <line
-        className="Axis__line"
-        x2={dimensions.boundedWidth}
-      />
+    <g
+      className="Axis AxisHorizontal"
+      transform={`translate(0, ${dimensions.boundedHeight})`}
+      {...props}
+    >
+      <line className="Axis__line" x2={dimensions.boundedWidth} />
 
       {ticks.map((tick) => (
-        <text
-          key={tick}
-          className="Axis__tick"
-          transform={`translate(${scale(tick)}, 25)`}
-        >
-          { formatTick(tick) }
-        </text>
+        <>
+          <line
+            className="Tick__line"
+            y1={0}
+            y2={5}
+            x1={scale(tick)}
+            x2={scale(tick)}
+          />
+          <text
+            key={tick}
+            className="Axis__tick"
+            transform={`translate(${scale(tick)}, 25)`}
+          >
+            {formatTick(tick)}
+          </text>
+        </>
       ))}
 
       {label && (
@@ -67,11 +72,11 @@ function AxisHorizontal ({ dimensions, label, formatTick, scale, ...props }) {
           className="Axis__label"
           transform={`translate(${dimensions.boundedWidth / 2}, 50)`}
         >
-          { label }
+          {label}
         </text>
       )}
     </g>
-  )
+  );
 }
 
 AxisHorizontal.propTypes = {
@@ -79,57 +84,64 @@ AxisHorizontal.propTypes = {
   scale: PropTypes.func,
   label: PropTypes.string,
   formatTick: PropTypes.func,
-}
+};
 
 AxisHorizontal.defaultProps = {
   scale: null,
   formatTick: d3.format(","),
-}
+};
 
-function AxisVertical ({ dimensions, label, formatTick, scale, ...props }) {
-  const numberOfTicks = dimensions.boundedHeight / 40
+function AxisVertical({ dimensions, label, formatTick, scale, ...props }) {
+  const numberOfTicks = dimensions.boundedHeight / 40;
 
-  const ticks = scale.ticks(numberOfTicks)
+  const ticks = scale.ticks(numberOfTicks);
 
   return (
     <g className="Axis AxisVertical" {...props}>
-      <line
-        className="Axis__line"
-        y2={dimensions.boundedHeight}
-      />
+      <line className="Axis__line" y2={dimensions.boundedHeight} />
 
       {ticks.map((tick) => (
-        <text
-          key={tick}
-          className="Axis__tick"
-          transform={`translate(-16, ${scale(tick)})`}
-        >
-          { formatTick(tick) }
-        </text>
+        <>
+          <line
+            className="Tick__line"
+            y1={scale(tick)}
+            y2={scale(tick)}
+            x1={0}
+            x2={-5}
+          />
+          <text
+            key={tick}
+            className="Axis__tick"
+            transform={`translate(-16, ${scale(tick)})`}
+          >
+            {formatTick(tick)}
+          </text>
+        </>
       ))}
 
       {label && (
         <text
           className="Axis__label"
           style={{
-            transform: `translate(-75px, ${dimensions.boundedHeight / 2}px) rotate(-90deg)`
+            transform: `translate(-75px, ${
+              dimensions.boundedHeight / 2
+            }px) rotate(-90deg)`,
           }}
         >
-          { label }
+          {label}
         </text>
       )}
     </g>
-  )
+  );
 }
 AxisVertical.propTypes = {
   dimensions: dimensionsPropsType,
   scale: PropTypes.func,
   label: PropTypes.string,
   formatTick: PropTypes.func,
-}
+};
 
 AxisVertical.defaultProps = {
   scale: null,
   formatTick: d3.format(","),
-}
-
+};
